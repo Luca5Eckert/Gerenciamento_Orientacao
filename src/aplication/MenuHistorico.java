325fab9c23c3d4e290dd7b88e4ec69afe0da2d1f
LucasEckert <ecklukatv@gmail.com>
2025-05-15 09:19:39 -0300

    feat: adicionado menu de historico dos menus

--- src/aplication/MenuHistorico.java
+++ src/aplication/MenuHistorico.java
@@ -6,4 +6,46 @@ import aplication.interfaces.Menu;
 
 public class MenuHistorico {
 	private List<Menu> linhaDoTempoMenu;
+	private int ponteiroDoMenu;
+	
+	public List<Menu> getLinhaDoTempoMenu();
+
+	public void setLnhaDoTempoMenu(List<Menu> novaLinhaDoTempo){
+		this.linhaDoTempoMenu = novaLinhaDoTempo;
+	}
+
+	public Menu pegarUltimoMenu(){
+		return linhaDoTempoMenu.getLast();
+	}
+
+	public Menu pegarMenuAtual(){
+		return this.linhaDoTempoMenu.get(this.ponteiroDoMenu);
+	}
+	
+	public Menu irProximoMenu(){
+		if(temProximo()){
+			this.ponteiroMenu++;
+			return pegarMenuAtual();
+		}
+
+		throw new Exception("Não existe proximo menu");
+	}
+
+	public Menu voltarMenu(){
+		if(temAnterior()){
+			this.ponteiroDoMenu--:
+			return pegarMenuAtual();
+		}
+
+		throw new Exception("Não existe menu anterior");
+	}
+
+	public boolean temProximo(){
+		return ponteiroAtual < linhaDoTempoMenu.lenght();
+	}
+
+	public boolean temAnterior(){
+		return ponteiroAtual > linhaDoTempoMenu.lenght();
+	}
+	
 }
