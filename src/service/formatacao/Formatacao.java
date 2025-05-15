45f0f7c3528fc5d362c875b03778f5cfe14815e4
LucasEckert <ecklukatv@gmail.com>
2025-05-15 08:42:08 -0300

    refactor: mudado o código da classe formatação

--- src/service/formatacao/Formatacao.java
+++ src/service/formatacao/Formatacao.java
@@ -3,6 +3,4 @@ package service.formatacao;
 public interface Formatacao {
 
     public String formatar(List<?> listaParaFormatar, IdiomaImplementacao idiomaImplementacao);
-
-    public void definirCaracteristica(Object objeto);
 }
